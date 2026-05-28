055d0b67fa179f5394befdab57584a8f657181a3
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2026-05-28 13:28:49 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:4000'
+      '/api': 'https://trillo-client.vercel.app'
     }
   }
 });
