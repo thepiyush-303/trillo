5a5da738f5bd4bd04cb6388d0a75d6a12ebb49b7
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2026-05-28 13:34:12 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      '/api': 'https://trillo-bqx1.onrender.com/'
+      '/api': 'http://localhost:4000'
     }
   }
 });
